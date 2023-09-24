cebd1f6623a6e0a52cb9d6a38ee5c518a72c16a2
Christine <Christine@DESKTOP-057HV8L>
2023-09-24 12:59:02 -0400

    PE9 Question #3

---
+++ PE9_Q3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PE9_Q3
+{
+    // Author: Christine Espeleta
+    // Purpose: Use delegate function to impersonate Console.ReadLine() when asking for user input.
+    // Restrictions: N/A
+
+    delegate string ReadLine();
+    internal class Program
+    {
+        // Purpose: Use delegate function to impersonate Console.ReadLine() when asking for user input.
+        // Restrictions: N/A
+        static void Main(string[] args)
+        {
+            string input = null;
+            Console.WriteLine("Please type a string.");
+            ReadLine consoleProcess;
+            consoleProcess = new ReadLine(report);
+            input = consoleProcess();
+            Console.WriteLine(input);
+        }
+        static string report()
+        {
+            return Console.ReadLine();
+        }
+    }
+}
