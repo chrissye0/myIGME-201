3d15ba0c6ce7efd465fcb06afdd9fc8b0f276571
Christine <Christine@DESKTOP-057HV8L>
2023-10-01 01:13:35 -0400

    Question #1 for Unit Test #1

---
+++ UnitTest1_test/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTest1_test
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double[][] dArray = new double[2][];
+            dArray[1] = new double[2];
+            dArray[0] = new double[1];
+
+            dArray[0][0] = 15;
+            dArray[1][0] = 5.67;
+        }
+    }
+}
